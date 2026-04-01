ee9cd6d3d4b39a2383b71c8eb218eead5bef63a6
dxtattoohunting <dxtattoohunting@gmail.com>
2026-04-01 16:54:46 +0530

    Create index.jsx

---
+++ pages/index.jsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <main style={{ minHeight: "100vh", background: "#000", color: "#fff", padding: "40px" }}>
+      <h1>Dx Tattoo Hunting Baroda</h1>
+      <p>Deployment test successful.</p>
+    </main>
+  );
+}
